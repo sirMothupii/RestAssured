f124d57fac3b8236434c2628e86c46cc2c5fc15e
sirMothupii <iamkeenan94@gmail.com>
2018-08-06 15:12:06 +0200

    Run CucumberWithSerenity

--- src/test/java/com/sqstraining/bddTestRunner.java
+++ src/test/java/com/sqstraining/bddTestRunner.java
@@ -2,12 +2,12 @@ package com.sqstraining;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
 import org.junit.runner.RunWith;
 
 //setting the path for the feature file
-@RunWith(Cucumber.class)
-@CucumberOptions(features = "classpath:features"
-        , glue = "com.sqstraining.requestweather.feature")
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(features = "classpath:requestweather.feature")
 
 public class bddTestRunner
 {
